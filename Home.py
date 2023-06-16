eb6d408cafca3c2b8bda272069249616d6775bf2
Supanida27 <enoey007@gmail.com>
2023-06-16 15:20:37 +0700

    .commit

--- Home.py
+++ Home.py
@@ -22,7 +22,7 @@ with col3:
 
    html_8 = """
 <div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>การทำนายข้อมูลดอกไม้</h5></center>
+<center><h5>การทำนายข้อมูล</h5></center>
 </div>
 """
 st.markdown(html_8, unsafe_allow_html=True)
