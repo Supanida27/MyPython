d613451d6bbca7b0cabec84ca366911c34ff1dfa
Supanida27 <enoey007@gmail.com>
2023-06-16 15:43:01 +0700

    .commit

--- Home.py
+++ Home.py
@@ -6,11 +6,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.header('Supanida')
-
-col1, col2, col3 = st.columns(3)
-
-#st.header("kairung")
+st.header('kairung')
 st.image("./pic/Supanida.jpg")
 col1, col2, col3 = st.columns(3)
 
@@ -26,7 +22,7 @@ with col3:
    st.header("Setosa")
    st.image("./pic/iris3.jpg")
 
-   html_8 = """
+html_8 = """
 <div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
@@ -42,23 +38,21 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
 
 if st.button("ทำนายผล"):
     #st.write("ทำนาย")
-    dt = pd.read_csv("./data/iris.csv") 
+   dt = pd.read_csv("./data/iris.csv") 
    X = dt.drop('variety', axis=1)
-   y = dt.variety
-   
+   y = dt.variety   
    Knn_model = KNeighborsClassifier(n_neighbors=3)
    Knn_model.fit(X, y)   
-
    x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
    st.write(Knn_model.predict(x_input))
    
    out=Knn_model.predict(x_input)
 
-  if out[0] == 'Setosa':
+   if out[0] == 'Setosa':
     st.image("./pic/iris1.jpg")
    elif out[0] == 'Versicolor':       
     st.image("./pic/iris2.jpg")
    else:
     st.image("./pic/iris3.jpg")
-   else:    
-    st.writ('ไม่ทำนาย')
+else:
+    st.write("ไม่ทำนาย")
